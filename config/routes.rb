632ec3febc86f12ca48f85c56977c55b0ba44758
maikimo <mike.p.monahan@gmail.com>
2018-05-29 12:34:50 -0400

    set routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  resources :home, only: [:index]
   resources :comments
   resources :packages
   resources :reviews
   resources :profiles
+  root 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
