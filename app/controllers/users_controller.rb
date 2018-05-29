79fbecb7aa16798b0330338bde8430f4be22999b
maikimo <mike.p.monahan@gmail.com>
2018-05-29 14:27:41 -0400

    email validation enabled in order to create user, added error messages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,6 @@ class UsersController < Clearance::UsersController
   private
 
   def user_params
-    params[:user].permit(:username, :password, :email)
+    params[:user].permit(:username, :password, :email, :email_confirmation)
   end
 end
