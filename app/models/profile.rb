0e18690cac6d63db055223037da8be9479d5e002
maikimo <mike.p.monahan@gmail.com>
2018-05-29 13:52:37 -0400

    fixed age error

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -2,5 +2,5 @@ class Profile < ApplicationRecord
   belongs_to :user
   validates :first_name, presence: true, format: { with: /\A[a-zA-Z]+\z/, message: "only allows letters" }
   validates :last_name, presence: true, format: { with: /\A[a-zA-Z]+\z/, message: "only allows letters" }
-  validates :age, presence: true, numericality: { integer_only: true, greater_than: 0, less_than: 100 }
+  validates :age, presence: true, numericality: { integer_only: true, greater_than: 18, less_than: 100 }
 end
