57f2fdfe97ff7aca4c81484e6987d78528296d89
maikimo <mike.p.monahan@gmail.com>
2018-05-29 13:50:02 -0400

    woops, meant validates

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,5 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :review
-  vindicates :content, presence: true, length: { minimum: 1, maximum: 150 }
+  validates :content, presence: true, length: { minimum: 1, maximum: 150 }
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -2,8 +2,8 @@ class Review < ApplicationRecord
   has_many :comments
   belongs_to :user
   belongs_to :package
-  vindicates :rating, presence: true, numericality: { numbers_only: true, in: 1..5 }
-  vindicates :general_content, presence: true, length: { minimum: 10, maximum: 300 }
-  vindicates :pros_content, presence: true, length: { minimum: 10, maximum: 100 }
-  vindicates :cons_content, presence: true, length: { minimum: 10, maximum: 100 }
+  validates :rating, presence: true, numericality: { numbers_only: true, in: 1..5 }
+  validates :general_content, presence: true, length: { minimum: 10, maximum: 300 }
+  validates :pros_content, presence: true, length: { minimum: 10, maximum: 100 }
+  validates :cons_content, presence: true, length: { minimum: 10, maximum: 100 }
 end
